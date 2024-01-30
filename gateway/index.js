accb6f9ead938599c8b649f84629246ad99ea92b
kaungkhantzawjudgify <kaung@judgify.me>
2024-01-30 16:47:41 +0630

    seperated service

--- gateway/index.js
+++ gateway/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use('/shopping', proxy('http://localhost:8003'))
 app.use('/customer', proxy('http://localhost:8001'))
-app.use('/', proxy('http://localhost:8002'))
+app.use('/', proxy('http://localhost:8002')) // product
 
 app.listen(8000, () => {
     console.log('Gateway is listening port 8000')
